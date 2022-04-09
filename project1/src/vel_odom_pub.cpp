863545a651c1bfb2a80d88b9b3c239fcc9480bbf
Peppe <simogabri1999@gmail.com>
2022-04-09 15:29:53 +0200

    Added skeleton for TF broadcasting

--- project1/src/vel_odom_pub.cpp
+++ project1/src/vel_odom_pub.cpp
@@ -3,16 +3,12 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/JointState.h>
+#include <tf/transform_broadcaster.h>
 
 
 float r = 0.7;
 float l = 0.2;
 float w = 0.169;
-
-// struct Wheel {
-//   float tick;
-//   float angVel;
-// }
 
 // Wheel wlb; //left bottom wheel
 // Wheel wlt; //left top wheel
@@ -25,7 +21,7 @@ ros::Publisher pub_vel;
 void CalcluateVelocityCallback(const sensor_msgs::JointState& msg_in){
 
   geometry_msgs::TwistStamped msg_out = geometry_msgs::TwistStamped();
-  msg_out.header.stamp = ros::Time(0);
+  msg_out.header.stamp = ros::Time::now();
 
   //msg_out.twist.linear.x
   //msg_out.twist.angular.x
@@ -42,13 +38,39 @@ void CalcluateOdometryCallback(const geometry_msgs::PoseStamped& msg_in){
   nav_msgs::Odometry msg_out = nav_msgs::Odometry();
 
   //msg_out.twist.linear.x
-  //msg_out.twist.angular.x
+  //msg_out.twist.angular.z
 
   //TODO ADD FORMULA 
 
   //pub_vel.publish(msg_out);
 
+  //BroadcastTF(msg_out.twist.linear.x, msg_out.twist.linear.y, msg_out.twist.angular.z);
+
   ROS_INFO("Ciao Odometry");
+}
+
+void BroadcastTF(double x, double y, double th)
+{
+  //http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
+
+  tf::TransformBroadcaster odom_broadcaster;
+
+  //since all odometry is 6DOF we'll need a quaternion created from yaw
+  geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+  //first, we'll publish the transform over tf
+  geometry_msgs::TransformStamped odom_trans;
+  odom_trans.header.stamp = ros::Time::now();
+  odom_trans.header.frame_id = "odom";
+  odom_trans.child_frame_id = "base_link";
+
+  odom_trans.transform.translation.x = x;
+  odom_trans.transform.translation.y = y;
+  odom_trans.transform.translation.z = 0.0;
+  odom_trans.transform.rotation = odom_quat;
+
+  //send the transform
+  odom_broadcaster.sendTransform(odom_trans);
 }
 
 int main(int argc, char **argv){
