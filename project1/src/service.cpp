cc0cc1eb78802e5a832b5a2bf857e17aef7205e0
Amor jr <amor.madhkour@mail.polimi.it>
2022-04-10 09:55:19 +0200

    added dependence and modified the code of the service but still not complete

--- project1/src/service.cpp
+++ project1/src/service.cpp
@@ -1,9 +1,20 @@
 #include "ros/ros.h"
 #include "Robotics/Reset.h"
+#include <nav_msgs/Odometry.h>
    
-bool reset_callback(int *count, pub_sub::Reset::Request &req, pub_sub::Reset::Response &res) 
+
+bool Turtle::teleportAbsoluteCallback(turtlesim::TeleportAbsolute::Request& req, turtlesim::TeleportAbsolute::Response&)
+{
+  teleport_requests_.push_back(TeleportRequest(req.x, req.y, req.theta, 0, false));
+  return true;
+}
+bool reset_callback(pub_sub::Reset::Request &req, pub_sub::Reset::Response &res) 
 {
    //todo implementing the settage of the new parameter
+   
+      float x = req.new_x;
+      float y = req.new_y;
+      float th = req.new_theta;
 
     return true;
 }
@@ -12,7 +23,7 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "reset_position_server");
     ros::NodeHandle n;
  
-    ros::ServiceServer service = n.advertiseService<pub_sub::Reset::Request, pub_sub::Reset::Response>("reset",boost::bind(&reset_callback, &count, _1, _2));
+    ros::ServiceServer service = n.advertiseService<pub_sub::Reset::Request, pub_sub::Reset::Response>("reset",boost::bind(&reset_callback, _1, _2));
     ROS_INFO("Ready to reset_pos.");
     ros::spin();
   
